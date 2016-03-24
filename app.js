8c16742896dda7abafb9f5f62f2d73176ec122b6
huayun321 <huayun321@gmail.com>
2016-03-24 12:00:38 +0800

    整理客户端api

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'bower_components')));
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
